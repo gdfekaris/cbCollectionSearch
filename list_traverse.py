6c52f9048f4a5dc22fd0d5b4916e6dcd06c4f07e
gdfekaris <gdfekaris@gmail.com>
2025-10-04 16:36:03 -0700

    added get_signed_issues and get_signed_issues_simplified

--- list_traverse.py
+++ list_traverse.py
@@ -76,6 +76,70 @@ def get_issues_simplified(series_name):
 
                print(issue_details)
 
+# Prints all signed issues (with details) in a specified series
+def get_signed_issues(series_name):
+   print("\nCollection of " + series_name + " issues:\n")
+   for series in collection:
+      for key, value in series.items():
+         if key == series_name:
+            for issue in value:
+               title = issue["title"]
+               issue_num = issue["issue"]
+               year = issue["year_published"]
+               variant = " ".join(map(str, issue["collectible_criteria"]["variant"]))
+               condition = issue["collectible_criteria"]["appraisal_status"] + " " + issue["collectible_criteria"]["condition"]
+
+               printing = ""
+               signed_by = ""
+               first_appearances = ""
+               other_notes = ""
+
+               issue_details = ""
+
+               if issue["print_run"] > 1:
+                  printing = issue["print_run"]
+                  lastDigit = int(repr(printing)[-1])
+                  if lastDigit == 2:
+                     printing = f"{printing}nd printing\n"
+                  if lastDigit == 3:
+                     printing = f"{printing}rd printing\n"
+                  if lastDigit in (4, 5, 6, 7, 8, 9, 0):
+                     printing = f"{printing}th printing\n"
+
+               if issue["collectible_criteria"]["signature"]["signed"]:
+                  signed_by = "signed by " + " and ".join(map(str, issue["collectible_criteria"]["signature"]["person"])) + " on " + " ".join(map(str, issue["collectible_criteria"]["signature"]["placement"])) + "\n"
+
+               if issue["collectible_criteria"]["significant_first_appearances"]:
+                first_appearances = "first appearances: " + ", ".join(map(str, issue["collectible_criteria"]["significant_first_appearances"])) + "\n"
+
+               if issue["collectible_criteria"]["other_notable_criteria"]:
+                other_notes = "other notes: " + ", ".join(map(str, issue["collectible_criteria"]["other_notable_criteria"])) + "\n"
+
+               issue_details =(title  + " #" + issue_num + "\n" +
+                "published in " + year + "\n" +
+                printing +
+                "variant: " + variant + "\n" +
+                "condition: " + condition + "\n" +
+                signed_by + first_appearances + other_notes
+               )
+
+               if issue["collectible_criteria"]["signature"]["signed"]:
+                  print(issue_details)
+
+# Prints all signed issues (without details) in a specified series
+def get_signed_issues_simplified(series_name):
+   print("\nCollection of " + series_name + " issues:\n")
+   for series in collection:
+      for key, value in series.items():
+         if key == series_name:
+            for issue in value:
+               title = issue["title"]
+               issue_num = issue["issue"]
+               
+               issue_details =(title  + " #" + issue_num)
+
+               if issue["collectible_criteria"]["signature"]["signed"]:
+                  print(issue_details)
 
 # Prints the name of each series in the collection, alphabetically
 def series_list():
@@ -126,6 +190,8 @@ def get_all():
 #Example function calls 
 #get_issues("The Amazing Spider-Man")
 #get_issues_simplified("The Amazing Spider-Man")
+#get_signed_issues("The Amazing Spider-Man")
+#get_signed_issues_simplified("The Amazing Spider-Man")
 #series_list()
 #get_all()
 
